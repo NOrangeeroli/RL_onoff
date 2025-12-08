716fafe3a2eb5ee12a9ea774cbe2a123d4755227
yamei <1842850321@qq.com>
2025-12-08 12:18:13 +0800

    debug

--- rl_onoff/sampling/__init__.py
+++ rl_onoff/sampling/__init__.py
@@ -1,6 +1,6 @@
 """Sampling utilities for LLM generation."""
 
-from rl_onoff.sampling.sampler import Sampler
+from rl_onoff.sampling.sampler import Sampler, SamplingConfig
 
-__all__ = ["Sampler"]
+__all__ = ["Sampler", "SamplingConfig"]
 
